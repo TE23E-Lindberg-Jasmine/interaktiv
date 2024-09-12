2dd250e9e05b209272bd17a24618453b34c507a6
jasmine lindberg <Jasmine.lindberg@elev.ga.ntig.se>
2024-09-12 13:08:28 +0200

    Inte klar

--- interaktiv/Program.cs
+++ interaktiv/Program.cs
@@ -1,15 +1,64 @@
-﻿Console.WriteLine("andvändarnamn");
+﻿using System.Xml.Serialization;
+
+Console.WriteLine("vart vill du hälst vara i skogen eller i stor stan.");
 string name = Console.ReadLine();
 
-if (name  == "Jasmine")
+if (name.ToLower() == "skogen")
 {
-  Console.WriteLine ("välkommen, du är jätte snygg")  ;
+  Console.Clear();
+  Console.WriteLine ("Mysss")  ; 
+  Console.ReadLine();
+
+  Console.WriteLine("Du har hamnat i en mysig skog, MEN du har hittat en person som har fått en hjärtattack, han ser ut som en rik person. Du får välja om du vill rädda den rika människan eller att råna honom.");
+
+String människa = Console.ReadLine();
+
+if (människa.ToLower() == "Råna")
+
+Console.Clear();
+Console.WriteLine ("Personen överlevde och kommer hemsöka dig när du dushar");
+Console.ReadLine();
+
+Console.WriteLine("Du hamnade i stor stan. Du hitter en guld taka på marken. Tar du den och går till polisen eller tar du den för dig själv?, skriv själv eller polis");
+string själv = Console.ReadLine ();
+
+if (name.ToLower () == "Sparka")
+
+  Console.Clear();
+  Console.WriteLine ("Aina kommer och tar dig, Ingen kommer undan, MUHAHAHAH")  ; 
+  Console.ReadLine();
+
+
+
+Console.WriteLine("")
+
+
+
+
+
 }
+
+
 else
 {
-Console.WriteLine("VEM ÄR DU?!?!?!?, BITCHHH");
-}
 
-
+Console.WriteLine("Ett ställe med mycket folk");
 Console.ReadLine();
+
+Console.WriteLine("Du håller på att bli rånad.Du får chansen att sparka rånaren. vad väljer du sparka eller inte ");
+Console.ReadLine();
+
+Console.WriteLine("Du försökte");
+Console.ReadLine();
+
+
+
+
+ }
+
+
+
+
+
+
 
